db01937b7ab288145d29dae282dd2690566bd8cc
Mahmood Nalim <mahmoodnalim@gmail.com>
2018-10-09 22:26:55 +0530

    space issue

--- ngApp/src/app/token-interceptor.service.ts
+++ ngApp/src/app/token-interceptor.service.ts
@@ -1,20 +1,19 @@
-import { Injectable , Injector } from '@angular/core';
-import { HttpInterceptor} from '@angular/common/http';
-import { AuthService} from './auth.service'
+import { Injectable, Injector } from "@angular/core";
+import { HttpInterceptor } from "@angular/common/http";
+import { AuthService } from "./auth.service";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class TokenInterceptorService implements HttpInterceptor {
+  constructor(private injector: Injector) {}
 
-  constructor(private injector : Injector) { }
-
-  intercept(req,next){
-    let authService = this.injector.get(AuthService)
+  intercept(req, next) {
+    let authService = this.injector.get(AuthService);
     let tokenizedReq = req.clone({
-      setHeaders:{
-        Authorization :`Bearer ${authService.getToken()}`
+      setHeaders: {
+        Authorization: `Bearer ${authService.getToken()}`
       }
-    })
-    return next.handle(tokenizedReq)
+    });
+    return next.handle(tokenizedReq);
   }
 }
