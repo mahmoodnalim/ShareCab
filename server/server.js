b04ca52afc93c73428d605ffcb77cba86a26787e
Mahmood Nalim <mahmoodnalim@gmail.com>
2018-12-11 18:23:42 +0530

    Angular server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 var http = require("http");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const api = require("./routes/api");
 const app = express();
 require("./startup/prod")(app);
@@ -16,8 +16,14 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use("/api", api);
 
+app.use(express.static(__dirname + "/dist"));
+
 app.get("/", function(req, res) {
   res.send("Helo from server");
+});
+
+app.all("*", (req, res) => {
+  res.status(200).sendfile(__dirname + "/dist/index.html");
 });
 
 app.listen(PORT, function() {
